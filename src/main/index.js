62c52debad612b18a01f34fefbbb9ec9854f4f6c
德拉费尔 <liudan92221@qq.com>
2020-12-07 01:51:02 +0800

    接口调试

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,4 @@
-import { Form, Input, DatePicker, Row, Col } from 'antd';
+import { Form, Input, DatePicker, Row, Col, Button } from 'antd';
 import moment from 'moment';
 
 const { RangePicker } = DatePicker;
@@ -11,7 +11,33 @@ function paramFormat(param) {
   return str.slice(0, str.length - 1)
 }
 
+function getData({
+  scode,
+  sdate,
+  edate
+}) {
+  return fetch(`http://webapi.cninfo.com.cn/api/stock/p_stock2300${paramFormat({
+    scode: scode,
+    type: '071001',
+    source: '033003',
+    sdate: sdate,
+    edate: edate,
+    access_token: ''
+  })}`, {
+    method: 'GET',
+    // credentials: 'include',
+    // mode: 'cors',
+  }).then((data) => {
+    return data.json()
+  }).then((data) => {
+    return data
+  })
+}
+
 const dateFormat = 'YYYY';
+
+const endDate = moment()
+const startDate = moment(endDate.format('YYYY') - 5, dateFormat)
 
 function Main() {
   const [form] = Form.useForm()
@@ -40,33 +66,52 @@ function Main() {
               </Form.Item>
             </Col>
             <Col span={8}>
-              <RangePicker
-                picker="year"
-                defaultValue={[moment('2015', dateFormat), moment('2020', dateFormat)]}
-                disabled={[false, true]}
-              />
+              <Form.Item
+                name="date"
+                label="日期"
+                initialValue={[startDate, endDate]}
+              >
+                <RangePicker
+                  picker="year"
+                  disabled={[false, true]}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]}>
+            <Col span={8}>
+              <Button
+                type="primary"
+                onClick={() => {
+                  const scode = form.getFieldValue('code')
+                  const date = form.getFieldValue('date')
+                  if (scode && date[0] && date[1]) {
+                    const sdate = date[0].format('YYYY') + '0101'
+                    const edate = date[1].format('YYYY') + '1231'
+                    getData({
+                      scode,
+                      sdate,
+                      edate
+                    }).then((data) => {
+                      console.log(data)
+                    })
+                  }
+                }}
+              >
+                资产负债表分析
+              </Button>
+              <Button
+                type="primary"
+                onClick={() => {
+                  form.resetFields(['code', 'date'])
+                }}
+              >
+                重置
+              </Button>
             </Col>
           </Row>
         </Form>
       </div>
-      <div onClick={() => {
-        fetch(`http://webapi.cninfo.com.cn/api/stock/p_stock2300${paramFormat({
-          scode: '000002',
-          type: '071001',
-          source: '033003',
-          sdate: '20170101',
-          edate: '20191231',
-          access_token: ''
-        })}`, {
-          method: 'GET',
-          // credentials: 'include',
-          // mode: 'cors',
-        }).then((data) => {
-          return data.json()
-        }).then((data) => {
-          console.log(data)
-        })
-      }}>xxx</div>
     </div>
   );
 }
